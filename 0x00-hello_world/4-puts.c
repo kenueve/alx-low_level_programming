44b05302fbfc4f608a6bb33be6fa4631be748682
Karim <karimelkahla@gmail.com>
2023-02-16 12:46:40 -0800

    maybe maybe

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* This program prints the following */
+/* This program prints the following 
+ *main : Entry point
+ *Return :0 
+ *
+ * */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
