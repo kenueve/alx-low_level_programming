016560264f656aee1e0d0a29b81b731a5e589035
Karim <karimelkahla@gmail.com>
2023-02-16 13:35:40 -0800

    nn

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
 /**
- * Description : a C program that prints with printf function
- * main: Entry point
- * return: Always 0 (Success)
- */
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
