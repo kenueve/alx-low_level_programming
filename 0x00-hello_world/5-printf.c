49e71bb86ae234b15cd4847945618bde4c3a1fff
Karim <karimelkahla@gmail.com>
2023-02-16 13:27:50 -0800

    aa

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/*Description : a program that prints the following
+ * main(void): indicate that the function takes no arguments
+ * return: Always 0 (Success)
+ * */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
