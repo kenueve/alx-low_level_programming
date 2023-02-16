ad88e7fb812b2ee7afefa2c70df0a49fbd43952e
Karim <karimelkahla@gmail.com>
2023-02-16 14:29:27 -0800

    maybe

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: a C program that prints the size of various types on the computer it is
- * compiled and run on.
+ * Description: a C program that prints the size of various types
  *
  * Return: Always 0 (Success)
  */
@@ -15,6 +14,6 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
-
-    return (0);
+	
+	return (0);
 }
