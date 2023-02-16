eab54bbbf7cba103c892b10be6558ac558d58bf0
Karim <karimelkahla@gmail.com>
2023-02-16 13:30:46 -0800

    !wut?

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/*Description : a program that prints the following
+/**
+ * Description : a program that prints the following
  * main(void): indicate that the function takes no arguments
  * return: Always 0 (Success)
- * */
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
