04599a58f51f3174dbdf6005dea0d2f71ec72007
Karim <karimelkahla@gmail.com>
2023-02-17 10:22:32 -0800

    second try

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program that prints single digit numbers from base 10
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i\n", i);
+	}
+	return (0);
+}
