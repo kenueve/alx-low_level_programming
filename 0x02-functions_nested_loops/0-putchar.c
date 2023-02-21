5af9cd899f21a5292ec0d23e6c46604890c51cff
Karim <karimelkahla@gmail.com>
2023-02-21 04:44:53 -0800

    first try

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+
+/**
+ *
+ * main: Entry point
+ *
+ * Description: printing _putchar without the use of stdio.h
+ *
+ *
+ *return: zero success
+ */
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+	return 0;
+}
