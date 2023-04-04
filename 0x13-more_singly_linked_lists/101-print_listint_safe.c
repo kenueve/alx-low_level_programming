ddb1918f853fbd1a08e4da23e2cacd7bd0a23cc1
Karim <karimelkahlakarim@gmail.com>
2023-04-04 07:03:21 -0700

    checking

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,40 +1,71 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a listint_t linked list
- * @head: pointer to the head of the list
+ * free_listp - frees a linked list
+ * @head: head of a list.
  *
- * Return: number of nodes in the list
+ * Return: no return.
+ */
+void free_listp(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curs;
+
+	if (head != NULL)
+	{
+		curs = *head;
+		while ((temp = curs) != NULL)
+		{
+			curs = curs->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
+ *
+ * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	const listint_t *slow = head, *fast = head;
+	size_t ndz = 0;
+	listp_t *hptr, *new, *add;
 
-	while (slow != NULL && fast != NULL && fast->next != NULL)
+	hptr = NULL;
+	while (head != NULL)
 	{
-		printf("[%p] %d\n", (void *) slow, slow->n);
-		slow = slow->next;
-		fast = fast->next->next;
-		count++;
-		if (slow == fast)
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
 		{
-			printf("[%p] %d\n", (void *) slow, slow->n);
-			count++;
-			break;
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (ndz);
+			}
 		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		ndz++;
 	}
 
-	if (slow == NULL || fast == NULL || fast->next == NULL)
-	{
-		while (head != NULL)
-		{
-			printf("[%p] %d\n", (void *) head, head->n);
-			head = head->next;
-			count++;
-		}
-	}
-
-	return (count);
+	free_listp(&hptr);
+	return (ndz);
 }
