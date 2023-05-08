65b55066c4de4ddb787b163c9ac1b75a4273957b
Karim <kenueve@Ubuntu22.04.myguest.virtualbox.org>
2023-05-08 12:20:21 +0100

    append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * APPEND_TEXT_TO_FILE - APPEND TEXT
+ * @filename: FILENAME.
+ * @text_content: ADDING CONT.
+ *
+ * RETURN: 1 IF FILE EXISTS. OTHER -1
+ * 
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
